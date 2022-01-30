372aae91c4f177fd17146e750b87d9d784cc3e12
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2022-01-30 20:28:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 # 添加旁路由防火墙
-echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+# echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 # 修改主机名
 echo '修改机器名称'
 sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
